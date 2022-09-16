1c2dbc52f6fa609c50fa58aa23a0bafa610046b8
David Genesis Pedeglorio <pedegloriodavidgenesis@gmail.com>
2022-09-16 12:17:19 +0800

    Complete footer

--- vite-project/src/Footer.jsx
+++ vite-project/src/Footer.jsx
@@ -1,8 +1,18 @@
-
+import facebookLogo from './assets/Facebook Icon.png'
+import twitterLogo from './assets/Twitter Icon.png'
+import instagramLogo from './assets/Instagram Icon.png'
+import linkedInLogo from './assets/LinkedIn Icon.png'
+import gitHublogo from './assets/GitHub Icon.png'
 
 export default function Footer() {
     return (
-        <footer></footer>
+        <footer>
+            <img src={twitterLogo} alt="Twitter logo" />
+            <img src={facebookLogo} alt="Facebook logo" />
+            <img src={instagramLogo} alt="Instagram logo" />
+            <img src={linkedInLogo} alt="LinkedIn logo" />
+            <img src={gitHublogo} alt="GitHub logo" />
+        </footer>
     )
         
 }
